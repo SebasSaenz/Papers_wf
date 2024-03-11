f0f739c429b2faa945129e6cb41e3977daa1248d
Johan Sebastián Sáenz <53427119+SebasSaenz@users.noreply.github.com>
2024-03-11 15:10:44 +0100

    plot peptides

---
+++ conservation_protocol/code/clean_metadata.R
@@ -0,0 +1,53 @@
+set.seed(150988)
+
+# Load libraries ---------------------------------------------------------------
+library(tidyverse)
+
+# Load data --------------------------------------------------------------------
+metadata <- read_tsv("conservation_protocol/rawdata/metadata.txt")
+
+# Clean metadata ---------------------------------------------------------------
+clean_meta <- metadata %>% 
+  mutate(treatment = str_replace(treatment, "W-0.", "FF-0a")) %>% 
+  separate(treatment,
+           into = c("sampling", "treatment", "incubation", "substrate"),
+           sep = "-") %>% 
+  mutate(treatment = case_when(
+    treatment == "W" ~ "C",
+    treatment == "FD" ~ "FDN",
+    treatment == "FD_noN" ~ "FD",
+    treatment == "F" ~ "FR",
+    treatment == "FN" ~ "FRN",
+    treatment == "FF" ~ "F",
+    .default = as.character(treatment)),
+    incubation = case_when(incubation == "1" ~ "Pre-incubation",
+                           incubation == "0a" ~ "No incubation",
+                           .default = as.character(incubation)),
+    substrate = case_when(substrate == "079" ~ "Wheat grain",
+                          substrate == "057" ~ "Rapeseed meal",
+                          substrate == "053" ~ "Maize grain",
+                          substrate == "Heu" ~ "Hay",
+                          substrate == "KF" ~ "Concentrate",
+                          is.na(substrate) ~ "None"),
+    incubation = factor(incubation,
+                        levels = c("No incubation", "Pre-incubation", "8", "24", "72")),
+    treatment = factor(treatment,
+                       levels = c("F", "C", "FR", "FRN", "FD", "FDN", "FD_noN3Wo")),
+    substrate = factor(substrate,
+                       levels = c("None", "Hay", "Rapeseed meal", "Wheat grain",
+                                  "Maize grain", "Concentrate"))) %>% 
+  filter(sample != "49" & sample != "78" & sample != "87",
+         treatment != "FD_noN3Wo") #Remove samples with 72h
+
+# Save clean metadata ----------------------------------------------------------
+saveRDS(clean_meta, file = "conservation_protocol/output/clean_meta.rds")
+
+
+
+# Clean Peptides file
+peptides <- read_tsv("conservation_protocol/rawdata/final_peptides.tsv") %>% 
+  select(Proteins, contains("Intensity ")) %>% 
+  filter(!grepl("SHEEP", Proteins))
+
+# Save clean metadata ----------------------------------------------------------
+saveRDS(peptides, file = "conservation_protocol/output/peptides.rds")
--- conservation_protocol/code/sum_peptides.R
+++ conservation_protocol/code/sum_peptides.R
@@ -1,48 +1,115 @@
-metadata <- read_tsv("conservation_protocol/rawdata/metadata.txt")
+set.seed(150988)
 
-clean_meta <- metadata %>%
-  separate(treatment,
-           into = c("sampling", "treatment", "incubation", "substrate"),
-           sep = "-") %>% 
-  mutate(incubation = str_replace(incubation, "1", "Pre-treated"),
-         incubation = str_replace(incubation, "0[a-b]", "No incubation"),
+# Load libraries ---------------------------------------------------------------
+library(tidyverse)
+library(patchwork)
+library(ggtext)
+
+
+# Load data --------------------------------------------------------------------
+clean_metadata <- read_rds("conservation_protocol/output/clean_meta.rds") %>% 
+  filter(!treatment == "F")
+
+peptides <- read_rds("conservation_protocol/output/peptides.rds")
+
+base_color <- c('#d8b365','#67a9cf', '#2166ac','#91cf60','#1a9850')
+
+#
+df <- peptides %>%
+  pivot_longer(-Proteins, names_to = "sample", values_to = "Intensity") %>%
+  group_by(sample) %>%
+  summarise(sum_intensity = sum(Intensity)) %>%
+  mutate(
+    log2 = log10(sum_intensity),
+    sample = str_remove(sample, "Intensity Seifert_Hammel_Nr"),
+    sample = str_remove(sample, "_WDH"),
+    sample = str_remove(sample, "_STneu"),
+    sample = str_remove(sample, "_wdh"),
+    sample = str_remove(sample, "_neuST"),
+    sample = as.numeric(sample)
+  ) %>%
+  inner_join(clean_metadata, by = "sample")
+
+# Statistical test -------------------------------------------------------------
+model <- aov(log2 ~ treatment * incubation * substrate, data = df)
+x <- summary(model)
+
+anova_results <- tidy(model)
+write_tsv(anova_results, file = "conservation_protocol/output/anova_peptides.txt")
+
+q# Plot ------------------------------------------------------------
+plot_incubation <- df %>% 
+  mutate(incubation = str_replace(incubation, "-", "<br>"),
          incubation = factor(incubation,
-                             levels = c("No incubation", "Pre-treated", "8", "24", "72")),
-         substrate = if_else(is.na(substrate), "None", substrate),
-         treatment = factor(treatment,
-                            levels = c("C", "W", "F", "FN", "FD", "FD_noN", "FD_noN3Wo")))
+                             levels = c("Pre<br>incubation", "8", "24"))) %>% 
+    ggplot(aes(
+      x = incubation,
+      y = log2
+    )) +
+    geom_boxplot(width = 0.5) +
+    geom_jitter(aes(colour = treatment),
+                width = 0.2,
+                alpha = 0.8
+    ) +
+    scale_y_continuous(
+      limits = c(10.90, 11.30),
+      breaks = seq(10.90, 11.30, 0.05)
+    ) +
+    scale_color_manual(values = base_color) +
+    labs(
+      x = NULL,
+      y = "Peptides intensity (Log2)"
+    ) +
+    theme(
+      panel.background = element_blank(),
+      legend.position = "none",
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_rect(colour = "black", fill = NA, size = 1),
+      axis.text.x = element_markdown()
+    )
 
-peptides <- read_tsv("conservation_protocol/rawdata/final_peptides.tsv") %>% 
-  select(Proteins, contains("Intensity "))
+plot_substrate <- df %>%
+  mutate(substrate = str_replace(substrate, "-", "<br>"),
+         substrate = str_replace(substrate, " ", "<br>"),
+         substrate = factor(substrate,
+                             levels = c("None", "Hay", "Rapeseed<br>meal", "Wheat<br>grain"))) %>% 
+  ggplot(aes(
+    x = substrate,
+    y = log2
+  )) +
+  geom_boxplot(width = 0.6) +
+  geom_jitter(aes(colour = treatment),
+              width = 0.2,
+              alpha = 0.8
+  ) +
+  scale_y_continuous(
+    limits = c(10.90, 11.30),
+    breaks = seq(10.90, 11.30, 0.05)
+  ) +
+  scale_color_manual(values = base_color) +
+  labs(
+    x = NULL,
+    y = "Peptides intensity (Log2)"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    legend.key = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 12),
+    legend.box.spacing  = unit(0, "cm"), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, size = 1),
+    axis.text.x = element_markdown()
+  ) +
+  guides(color = guide_legend(override.aes = list(size = 3)))
 
-df <- peptides %>% 
-  pivot_longer(-Proteins, names_to = "sample", values_to = "Intensity") %>% 
-  group_by(sample) %>% 
-  summarise(sum_intensity = sum(Intensity)) %>% 
-  mutate(log2 = log10(sum_intensity),
-         sample = str_remove(sample, "Intensity Seifert_Hammel_Nr"),
-         sample = str_remove(sample, "_WDH"),
-         sample = str_remove(sample, "_STneu"),
-         sample = str_remove(sample, "_wdh"),
-         sample = str_remove(sample, "_neuST"),
-         sample = as.numeric(sample)) %>% 
-  inner_join(clean_meta, by = "sample") %>% 
-  filter(treatment != "FD_noN3Wo")
+compose_plot <- plot_incubation + plot_substrate + plot_layout(axes = "collect") +
+  plot_annotation(tag_levels = 'A') & theme(plot.tag = element_text(size = 12))
 
-
-model <- aov(log2 ~ treatment * incubation * substrate, data=df)
-
-summary(model)
-
-
-df %>% 
-  ggplot(aes(x = incubation,
-             y = log2)) +
-  geom_boxplot()+
-  geom_jitter(aes(colour = substrate),
-              width = 0.2) +
-  scale_y_continuous(limits = c(11,11.30),
-                     breaks = seq(11, 11.30, 0.05)) +
-  labs(x = NULL,
-       y = "Peptides intensity (Log2)") +
-  theme_bw()
+# Save plot --------------------------------------------------------------------
+ggsave(compose_plot, file = "conservation_protocol/plots/peptides_plot.png",
+       dpi = 450,
+       width = 6.5,
+       height = 4)
